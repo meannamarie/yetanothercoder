1b8496113719e62f2b7c793014264f19793c58d4
Mikhail Baturov <mbaturov@vitpc.com>
2012-11-20 11:39:09 +0400

    logger tests

--- spring-hibernate-tests/src/main/java/ru/yetanothercoder/tests/springhibernate/Tests.java
+++ spring-hibernate-tests/src/main/java/ru/yetanothercoder/tests/springhibernate/Tests.java
@@ -1,7 +1,12 @@
 package ru.yetanothercoder.tests.springhibernate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Tests {
+    public static final Logger logger = LoggerFactory.getLogger(Tests.class);
+
     public static void main(String[] args) {
-        System.out.println("!");
+        logger.debug("debug from LogBack!");
     }
 }
